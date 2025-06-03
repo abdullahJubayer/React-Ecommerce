15e147b0360ce65da421d1c61bef07eca9282a81
jubayer <abdullahjubayer38@gmail.com>
2025-06-03 19:38:43 +0600

    added shimmer loading

--- src/components/InfiniteProductList.tsx
+++ src/components/InfiniteProductList.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useProductPagination } from "../hooks/useProductPagination";
 import { ProductItem } from "./ProductItem";
 import InfiniteScroll from "react-infinite-scroll-component";
+import { ProductItemShimmer } from "./ProductItemShimmer";
 
 export const InfiniteProductList: React.FC = () => {
   const [currentPage, setCurrentPage] = useState(0);
@@ -19,14 +20,11 @@ export const InfiniteProductList: React.FC = () => {
       hasMore={
         !loading && !error && (products ? products.length % 10 === 0 : false)
       }
-      loader={<h4>Loading...</h4>}
-      endMessage={
-        <p style={{ textAlign: "center" }}>
-          <b>Yay! You have seen it all</b>
-        </p>
-      }
+      loader={<h4 className="text-center">Loading...</h4>}
     >
       <div className="grid grid-cols-2 md:grid-cols-4 gap-4 md:gap-8 mx-4 md:mx-16 my-8">
+        {products.length === 0 &&
+          [1, 2, 3, 4].map((e) => <ProductItemShimmer key={e} />)}
         {products &&
           products.map((item) => <ProductItem key={item.id} item={item} />)}
       </div>
